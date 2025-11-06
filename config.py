ab8cb737818e9209ada8f86fd7453b114cb74fc7
ChristianSkou <christian140398@gmail.com>
2025-11-06 08:40:21 +0100

    added config of utput format

---
+++ config.py
@@ -0,0 +1,11 @@
+"""
+Configuration file for SAM Annotator
+Users can specify export format and other settings here
+"""
+
+# Export format: "voc" or "coco"
+EXPORT_FORMAT = "coco"  
+
+# COCO export settings (only used when EXPORT_FORMAT = "coco")
+COCO_CATEGORIES = None  
+
